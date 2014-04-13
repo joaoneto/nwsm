a6f7f93338a07c00a6bd055cd8fdb60c1d11c4a9
João Neto <joaopintoneto@gmail.com>
2014-04-13 17:52:15 -0300

    wip

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -7,7 +7,35 @@ Window.setTitle('João Neto');
 
 require('../lib/modal');
 
-$(Window.document).ready(function () {
-  Window.listProcesses();
-  setInterval(Window.listProcesses, 1000);
+// $(Window.document).ready(function () {
+//   Window.listProcesses();
+//   setInterval(Window.listProcesses, 1000);
+// });
+
+var State = require('../lib/state'),
+    Controller = require('../lib/controller');
+
+
+var teste = new Controller;
+
+teste.sub('before', function () {
+  setTimeout(function () {
+    console.log('subscribe before controller run 1');
+  }, 1000);
 });
+
+teste.sub('before', function () {
+  setTimeout(function () {
+    console.log('subscribe before controller run 2');
+  }, 1000);
+});
+
+teste.sub('after', function () {
+  console.log('subscribe after controller run 1');
+});
+
+teste.sub('after', function () {
+  console.log('subscribe after controller run 2');
+});
+
+teste.run();
